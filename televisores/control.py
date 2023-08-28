ee6a84b6b8854b0a5eecb6c09aa3bab476fbf854
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-08-28 14:00:21 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -16,20 +16,20 @@ class Control:
         return self._estado
     
     def canalUp (self):
-        if self._tv == True and self._canal < 120:
-            self._canal += 1
+        if self._tv != None:
+            return self._tv.canalUp()
         
     def canalDown (self):
-        if self._tv == True and self._canal > 0:
-            self._canal -= 1
+        if self._tv != None:
+            return self._tv.canalDown()
 
     def volumenUp (self):
-        if self._tv == True and self._volumen < 7:
-            self._volumen += 1
+        if self._tv != None:
+            return self._tv.volumenUp()
         
     def volumenDown (self):
-        if self._tv == True and self._volumen > 0:
-            self._volumen -= 1
+        if self._tv != None:
+            return self._tv.volumenDown()
     
     def setCanal (self, canal):
         self._canal = canal
