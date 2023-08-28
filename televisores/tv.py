17b4e7aba43a392786067fc3f904018c507bcc50
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-08-28 12:07:52 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,12 +1,12 @@
 class TV:
     _numTV = 0
     def __init__(self, marca, estado):
-        self._marca = Marca
+        self._marca = marca
         self._canal = 1
         self._precio = 500
         self._estado = estado
         self._volumen = 1
-        self._control = Control
+        self._control = Control()
         TV._numTV += 1
 
 
