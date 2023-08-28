9a2d17b7848c3767b8b3a230a907019f3545f125
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-08-27 23:47:39 -0500

    tv.py

---
+++ televisores/tv.py
@@ -0,0 +1,35 @@
+class TV:
+    _numTV = 0
+    def __init__(self, marca, estado):
+        self._marca = Marca
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control = Control
+        TV._numTV += 1
+
+
+    def setMarca (self, marca):
+        self._marca = marca
+    
+    def getMarca (self):
+        return self._marca
+
+    def getCanal (self):
+        return self._canal
+
+    def setPrecio (self, precio):
+        self._precio = precio
+
+    def getPrecio (self):
+        return self._precio
+    
+    def getVolumen (self):
+        return self._volumen
+    
+    def setControl (self, control):
+        self._control = control
+    
+    def getControl (self):
+        return self._control
